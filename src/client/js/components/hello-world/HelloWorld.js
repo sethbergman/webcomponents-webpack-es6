00c481c974e4f4e81d14757dfcd6e8ba450391c2
Seth Bergman <seth.atxwebs@gmail.com>
2017-06-10 21:29:28 -0500

    badges

--- src/client/js/components/hello-world/HelloWorld.js
+++ src/client/js/components/hello-world/HelloWorld.js
@@ -11,7 +11,7 @@ export default class HelloWorld extends HTMLElement {
 	constructor() {
 		super();
 		this._who = '';
-		// this._name = ''; //this property is bind to element attribute becouse of observedAttributes
+		this._name = ''; //this property is bind to element attribute becouse of observedAttributes
 	}
 
 	// static get observedAttributes() {
@@ -28,7 +28,7 @@ export default class HelloWorld extends HTMLElement {
 		//  this is handled by WebComponent decorator
 		if (this.hasAttribute('who')) {
 			this._who = this.getAttribute('who');
-			// this._name = this.getAttribute('name');
+			this._name = this.getAttribute('name');
 		}
 		this._updateRendering();
 	}
@@ -47,7 +47,7 @@ export default class HelloWorld extends HTMLElement {
 	_updateRendering() {
 		if (this.shadowRoot) {
 			this.shadowRoot.querySelector("#who").textContent = `, ${this._who}`;
-			// this.shadowRoot.querySelector("#name").textContent = `, ${this._name}`;
+			this.shadowRoot.querySelector("#name").textContent = `, ${this._name}`;
 		}
 	}
 }
--- src/server/render.js
+++ src/server/render.js
@@ -2,7 +2,7 @@ export function createServerRenderer(htmlTemplate) {
   return () =>
     new Promise((resolve, reject) => {
       try {
-        const appHtml = "<hello-world></hello-world>";
+        const appHtml = "<hello-world></hello-world><hello-name></hello-name>";
         const html = htmlTemplate.replace(
           /(<div\s*id="app">)(<\/div>)/,
           `$1${appHtml}$2`
